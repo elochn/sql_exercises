c2dc5f82fd748a83403b0aa0a5c71c97047700ea
elochn <94411557+elochn@users.noreply.github.com>
2025-03-12 23:31:28 +0100

    Update the SQL format

--- Northwind/Medium/Northwind_Medium_2.sql
+++ Northwind/Medium/Northwind_Medium_2.sql
@@ -1,6 +1,8 @@
 /* Show the category_name and the average product unit price for each category rounded to 2 decimal places. */
 
-SELECT category_name, ROUND(AVG(unit_price),2) from products 
-join categories 
-on categories.category_id = products.category_id
-GROUP BY category_name
+SELECT category_name,
+       Round(Avg(unit_price), 2)
+FROM   products
+       JOIN categories
+         ON categories.category_id = products.category_id
+GROUP  BY category_name 
--- Northwind/Medium/Northwind_Medium_3.sql
+++ Northwind/Medium/Northwind_Medium_3.sql
@@ -1,5 +1,6 @@
 /* Show the city, company_name, contact_name from the customers and suppliers table merged together.
 Create a column which contains 'customers' or 'suppliers' depending on the table it came from. */
+
 SELECT city,
        company_name,
        contact_name,
