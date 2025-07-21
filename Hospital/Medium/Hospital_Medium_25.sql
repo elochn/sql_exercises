600249e263807301224b7563f67eb602915671b1
elochn <94411557+elochn@users.noreply.github.com>
2025-07-21 12:33:12 +0200

    Add exercise

---
+++ Hospital/Medium/Hospital_Medium_25.sql
@@ -0,0 +1,3 @@
+/*Show patient_id, first_name, last_name from patients whose does not have any records in the admissions table. 
+(Their patient_id does not exist in any admissions.patient_id rows.)*/
+
